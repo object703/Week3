3a74caa64bd7c524a862216a9c0f3012f2c48c6d
object703 <24558755+object703@users.noreply.github.com>
2025-02-12 16:33:17 +0000

    constructors

---
+++ Week3/ArrayOfNumbers.cs
@@ -0,0 +1,21 @@
+﻿namespace Week3
+{
+    internal class ArrayOfNumbers
+    {
+        int[] _numbers;
+
+        // Create array of specified length with each value 0
+        public ArrayOfNumbers(int length)
+        {
+            // https://stackoverflow.com/a/76573590
+            _numbers = Enumerable.Repeat(0, length).ToArray();
+        }
+
+        // Create array with specified values
+        public ArrayOfNumbers(int[] numbers)
+        {
+            // https://learn.microsoft.com/en-us/dotnet/api/system.array.clone?view=net-8.0
+            _numbers = (int[])numbers.Clone();
+        }
+    }
+}
